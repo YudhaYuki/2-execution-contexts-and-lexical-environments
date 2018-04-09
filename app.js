dff8424dc15ef0d85ffa4fa1ce52d152def9c65a
YudhaYuki <YudhaYuki@hotmail.com>
2018-04-09 22:17:40 +0200

    The conceptual Aside: Javascript and Undefined

---
+++ app.js
@@ -0,0 +1,24 @@
+
+// b();
+// console.log(a);
+
+/*
+var a = 'Hello world';
+
+function b() {
+    console.log('Called b!');
+}
+
+b();
+console.log(a);
+*/
+
+
+var a;
+console.log(a);
+
+if (a === undefined) {
+    console.log('a is undefined');
+} else {
+    console.log('a is defined');
+}
